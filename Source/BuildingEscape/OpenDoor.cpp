7a58bb7f3aa526bf85a1774c4746112315fd0310
LAPTOP-7D3J5NDD\artco <artcodex@gmail.com>
2022-01-08 14:31:38 -0700

    fixes

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -3,6 +3,8 @@
 
 #include "OpenDoor.h"
 #include "GameFramework\Actor.h"
+#include "GameFramework\PlayerController.h"
+#include "Engine\World.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -21,11 +23,8 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	float rotation = 90.f;
-	FRotator rotator = GetOwner()->GetActorRotation();
-	FRotator newRotator = FRotator(rotator.Pitch, rotation, rotator.Yaw);
-
-	GetOwner()->SetActorRotation(newRotator);
+	initialYaw = GetOwner()->GetActorRotation().Yaw;
+	targetYaw = initialYaw;
 }
 
 
@@ -35,5 +34,60 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	//float rotation = 90.f;
+	//FRotator rotator = GetOwner()->GetActorRotation();
+	//FRotator newRotator = FRotator(rotator.Pitch, rotation, rotator.Yaw);
+
+	//GetOwner()->SetActorRotation(newRotator);
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("%f"), GetOwner()->GetActorRotation().Yaw);
+	//UE_LOG(LogTemp, Warning, TEXT("%f"), GetOwner()->GetActorRotation().Roll);
+
+	AActor* testActor = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (pressurePlate != nullptr &&
+		testActor != nullptr) {
+		if (pressurePlate->IsOverlappingActor(testActor)) {
+			//UE_LOG(LogTemp, Warning, TEXT("overlapping"));
+			targetYaw = initialYaw + openYaw;
+			currentSpeed = openSpeed;
+		}
+		else {
+			//UE_LOG(LogTemp, Warning, TEXT("not overlapping"));
+			targetYaw = initialYaw;
+			currentSpeed = closeSpeed;
+		}
+
+		SwingDoor(DeltaTime);
+	}
+
+	HandleOpenClose();
+}
+
+void UOpenDoor::SwingDoor(float DeltaTime) {
+	float currentYaw = GetOwner()->GetActorRotation().Yaw;
+
+	if (FMath::Abs(currentYaw - targetYaw) > 0.001f) {
+		float nextYaw = FMath::FInterpTo(currentYaw, targetYaw, DeltaTime, currentSpeed);
+
+		FRotator newRotator = FRotator(0.f, nextYaw, 0.f);
+		GetOwner()->SetActorRotation(newRotator);
+	}
+}
+ 
+void UOpenDoor::HandleOpenClose() {
+	UWorld* world = GetWorld();
+	APlayerController* playerController = NULL;
+	for (FConstPlayerControllerIterator Iterator = world->GetPlayerControllerIterator(); Iterator; ++Iterator) {
+		playerController = Iterator->Get();
+		break;
+	}
+
+	if (playerController->WasInputKeyJustPressed(EKeys::C)) {
+		targetYaw -= openYaw;
+	}
+	else if (playerController->WasInputKeyJustPressed(EKeys::O)) {
+		targetYaw += openYaw;
+	}
 }
 
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -23,6 +24,27 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	float targetYaw = 0.f;
+	float initialYaw = 0.0f;
 
-		
+	UPROPERTY(EditAnywhere)
+		float openYaw = 90.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* pressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	AActor* PlayerActor;
+
+	UPROPERTY(EditAnywhere)
+	float openSpeed = 2.f;
+
+	UPROPERTY(EditAnywhere)
+	float closeSpeed = 2.f;
+
+	float currentSpeed = openSpeed;
+private: 
+	void HandleOpenClose();
+	void SwingDoor(float DeltaTime);
 };
